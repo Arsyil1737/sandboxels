1ca76772cb88b6ac0a5c973a005a158c20d28049
Arsyil1737 <lelya.rahmat@gmail.com>
2025-09-05 16:21:29 +0700

    Update pushers.js

--- mods/pushers.js
+++ mods/pushers.js
@@ -1,11 +1,11 @@
 elements.up_pusher = {
 	color: "#9fafdf",
 	properties: {
-		range: 10,
+		range: 1,
 		pushStrength: 1,
 	},
 	tick: function(pixel) { 
-		pixel.range ??= 10;
+		pixel.range ??= 1;
 		pixel.pushStrength ??= 1;
 
 		for(h = 0; h < pixel.pushStrength; h++) {
@@ -19,11 +19,11 @@ elements.up_pusher = {
 		doDefaults(pixel);
 	},
 	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
+	breakInto: ,
+	tempHigh: ,
+	stateHigh: ,
 	density: 10000,
-	hardness: 0.85,
+	hardness: 1,
 	conduct: 1,
 	state: "solid",
 }
@@ -31,11 +31,11 @@ elements.up_pusher = {
 elements.down_pusher = {
 	color: "#9fafdf",
 	properties: {
-		range: 10,
+		range: 1,
 		pushStrength: 1,
 	},
 	tick: function(pixel) { 
-		pixel.range ??= 10;
+		pixel.range ??= 1;
 		pixel.pushStrength ??= 1;
 
 			for(h = 0; h < pixel.pushStrength; h++) {
@@ -49,11 +49,11 @@ elements.down_pusher = {
 		doDefaults(pixel);
 	},
 	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum0", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
+	breakInto: ,
+	tempHigh: ,
+	stateHigh: ,
 	density: 10000,
-	hardness: 0.85,
+	hardness: 1,
 	conduct: 1,
 	state: "solid",
 }
@@ -61,11 +61,11 @@ elements.down_pusher = {
 elements.left_pusher = {
 	color: "#9fafdf",
 	properties: {
-		range: 10,
+		range: 1,
 		pushStrength: 1,
 	},
 	tick: function(pixel) { 
-		pixel.range ??= 10;
+		pixel.range ??= 1,
 		pixel.pushStrength ??= 1;
 
 		for(h = 0; h < pixel.pushStrength; h++) {
@@ -79,11 +79,11 @@ elements.left_pusher = {
 		doDefaults(pixel);
 	},
 	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
+	breakInto: [none],
+	tempHigh: [none],
+	stateHigh: [none],
 	density: 10000,
-	hardness: 0.85,
+	hardness: 1,
 	conduct: 1,
 	state: "solid",
 }
@@ -91,11 +91,11 @@ elements.left_pusher = {
 elements.right_pusher = {
 	color: "#9fafdf",
 	properties: {
-		range: 10,
+		range: 1,
 		pushStrength: 1,
 	},
 	tick: function(pixel) { 
-		pixel.range ??= 10;
+		pixel.range ??= 1;
 		pixel.pushStrength ??= 1;
 
 		for(h = 0; h < pixel.pushStrength; h++) {
@@ -109,179 +109,12 @@ elements.right_pusher = {
 		doDefaults(pixel);
 	},
 	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
+	breakInto: [none],
+	tempHigh: [none],
+	stateHigh: [none],
 	density: 10000,
-	hardness: 0.85,
+	hardness: 1,
 	conduct: 1,
 	state: "solid",
 }
 
-elements.up_e_pusher = {
-	color: "#9f9f6f",
-	properties: {
-		range: 10,
-		pushTime: 0,
-		pushLength: 5,
-		pushStrength: 1,
-	},
-	tick: function(pixel) { 
-		pixel.range ??= 10;
-		pixel.pushTime ??= 0;
-		pixel.pushLength ??= 5;
-		pixel.pushStrength ??= 1;
-		if(isNaN(pixel.pushTime) || pixel.pushTime < 0) { pixel.pushTime = 0 };
-
-		if(pixel.charge) {
-			pixel.pushTime = pixel.pushLength;
-		};
-
-		if(pixel.pushTime > 0) {
-			for(h = 0; h < pixel.pushStrength; h++) {
-				for(i=(pixel.range - 1); i>=0; i--) {
-					if (!isEmpty(pixel.x,pixel.y-1-i,true)) {
-						tryMove(pixelMap[pixel.x][pixel.y-1-i],pixel.x,pixel.y-2-i);
-					};
-				};
-			};
-			pixel.pushTime--;
-		};
-		
-		doDefaults(pixel);
-	},
-	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
-	density: 10000,
-	hardness: 0.85,
-	conduct: 1,
-	state: "solid",
-}
-
-elements.down_e_pusher = {
-	color: "#9f9f6f",
-	properties: {
-		range: 10,
-		pushTime: 0,
-		pushLength: 5,
-		pushStrength: 1,
-	},
-	tick: function(pixel) { 
-		pixel.range ??= 10;
-		pixel.pushTime ??= 0;
-		pixel.pushLength ??= 5;
-		pixel.pushStrength ??= 1;
-		if(isNaN(pixel.pushTime) || pixel.pushTime < 0) { pixel.pushTime = 0 };
-
-		if(pixel.charge) {
-			pixel.pushTime = pixel.pushLength;
-		};
-
-		if(pixel.pushTime > 0) {
-			for(h = 0; h < pixel.pushStrength; h++) {
-				for(i=(pixel.range - 1); i>=0; i--) {
-					if (!isEmpty(pixel.x,pixel.y+1+i,true)) {
-						tryMove(pixelMap[pixel.x][pixel.y+1+i],pixel.x,pixel.y+2+i);
-					};
-				};
-			};
-			pixel.pushTime--;
-		};
-
-		doDefaults(pixel);
-	},
-	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum0", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
-	density: 10000,
-	hardness: 0.85,
-	conduct: 1,
-	state: "solid",
-}
-
-elements.left_e_pusher = {
-	color: "#9f9f6f",
-	properties: {
-		range: 10,
-		pushTime: 0,
-		pushLength: 5,
-		pushStrength: 1,
-	},
-	tick: function(pixel) { 
-		pixel.range ??= 10;
-		pixel.pushTime ??= 0;
-		pixel.pushLength ??= 5;
-		pixel.pushStrength ??= 1;
-		if(isNaN(pixel.pushTime) || pixel.pushTime < 0) { pixel.pushTime = 0 };
-
-		if(pixel.charge) {
-			pixel.pushTime = pixel.pushLength;
-		};
-
-		if(pixel.pushTime > 0) {
-			for(h = 0; h < pixel.pushStrength; h++) {
-				for(i=(pixel.range - 1); i>=0; i--) {
-					if (!isEmpty(pixel.x-1-i,pixel.y,true)) {
-						tryMove(pixelMap[pixel.x-1-i][pixel.y],pixel.x-2-i,pixel.y);
-					};
-				};
-			};
-			pixel.pushTime--;
-		};
-
-		doDefaults(pixel);
-	},
-	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
-	density: 10000,
-	hardness: 0.85,
-	conduct: 1,
-	state: "solid",
-}
-
-elements.right_e_pusher = {
-	color: "#9f9f6f",
-	properties: {
-		range: 10,
-		pushTime: 0,
-		pushLength: 5,
-		pushStrength: 1,
-	},
-	tick: function(pixel) { 
-		pixel.range ??= 10;
-		pixel.pushTime ??= 0;
-		pixel.pushLength ??= 5;
-		pixel.pushStrength ??= 1;
-		if(isNaN(pixel.pushTime) || pixel.pushTime < 0) { pixel.pushTime = 0 };
-
-		if(pixel.charge) {
-			pixel.pushTime = pixel.pushLength;
-		};
-
-		if(pixel.pushTime > 0) {
-			for(h = 0; h < pixel.pushStrength; h++) {
-				for(i=(pixel.range - 1); i>=0; i--) {
-					if (!isEmpty(pixel.x+1+i,pixel.y,true)) {
-						tryMove(pixelMap[pixel.x+1+i][pixel.y],pixel.x+2+i,pixel.y);
-					};
-				};
-			};
-			pixel.pushTime--;
-		};
-
-		doDefaults(pixel);
-	},
-	category: "machines",
-	breakInto: ["metal_scrap", "steel", "iron", "glass", "uranium", "tin"],
-	tempHigh: 2400,
-	stateHigh: ["molten_aluminum", "molten_steel", "molten_iron", "molten_glass", "molten_uranium", "molten_tin"],
-	density: 10000,
-	hardness: 0.85,
-	conduct: 1,
-	state: "solid",
-}
